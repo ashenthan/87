ba6a567f8ec0645abe64e2a8858b61349f1fd591
ashenthan <68573593+ashenthan@users.noreply.github.com>
2022-01-08 11:47:41 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# 87
+# spectagram-stage-7
+boilerplate code for c87 spectagram app
+check feed.js
---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyD93Q0TuhlbKOSI5KBVFoBJwDA1_GiOkTU",
+    authDomain: "spectagram-cbac5.firebaseapp.com",
+    projectId: "spectagram-cbac5",
+    storageBucket: "spectagram-cbac5.appspot.com",
+    messagingSenderId: "972673989567",
+    appId: "1:972673989567:web:e4afb1b2613401f5b83f0a"
+  };
+  
